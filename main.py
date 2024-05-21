bb81687d163999e289cee2ff1f0ba70d8e05a045
prigeo <priscillapedrette@gmail.com>
2024-05-21 19:55:04 -0300

    Contador

---
+++ main.py
@@ -0,0 +1,7 @@
+# usuário informa um número
+contador = int(input('Informe um número inteiro: '))
+
+# conta a partir do número informado até 0
+while contador >= 0:
+    print(contador)
+    contador -= 1
